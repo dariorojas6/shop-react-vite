00cc28f748050ad986a1832daa0b7a5c536a36d6
dariorojas6 <dario43055@gmail.com>
2023-05-23 19:38:13 -0300

    cambios

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export const Pagination = () => {
+    
+    const pageNumbers = []
+
+    // for(let i = 1; i )
+    
+ 
+
     return (
         <nav className="pagination is-centered mb-6" role="navigation" aria-label="pagination">
             <a className="pagination-previous">Previous</a>
